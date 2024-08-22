0ee3120b25a033668983ef25ae867cf72d8e0805
estefano calero <estefano@alenasoft.com>
2024-08-22 11:21:19 -0400

    added user data in login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
-
-const router = express.Router();
-
 import pool from "../config/db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+const router = express.Router();
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
@@ -13,12 +11,14 @@ router.post("/login", async (req, res) => {
     const result = await pool.query("SELECT * FROM users WHERE username = $1", [
       username,
     ]);
+
     if (result.rows.length === 0) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
@@ -26,7 +26,11 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.status(200).json({ token });
+
+    res.status(200).json({
+      token,
+      user,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
